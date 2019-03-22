f84d63d1f58d25c617ce90ba56ac2a129514aef1
Elle Valentine <elle.valentine@hotmail.com>
2019-03-22 02:03:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,15 @@ katz_deli = []
 
 def line(katz_deli)
   
-  if katz_deli.size=0 return "The line is currently empty."
+  if katz_deli.size=0 
+    return "The line is currently empty."
   else 
     return katz_deli
 end
 end  
+
+
+def take_a_number(katz_deli, customer_name)
+
+puts 
+end
